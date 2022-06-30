c46b30f433843674b8f4a8687dd259be1672e89d
alekione <lexa9448@gmail.com>
2022-06-30 21:35:22 +0100

    modified:   4-puts.c 	modified:   5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
+#include	<stdio.h>
 /*
  * A program to print a string of words
+ * main : function
+ * return: when successful.
  */
-#include	<stdio.h>
-
-int main()
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
+#include <stdio.h>
 /*
  * A program that uses printf function 
  * to display a string on the screen.
  * main - fuction
  * return - return 0 if successful.
  */
-#include <stdio.h>
-int main()
+int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
