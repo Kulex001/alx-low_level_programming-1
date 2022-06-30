817031c2134973f3281fdbaf77c86e01874f5495
alekione <lexa9448@gmail.com>
2022-06-30 23:45:35 +0100

    modified:   6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /*
+ * main(void) - Entry point.
+ *
  * TA program that prints sizes  of various
  * types on the computer it is compiled
  * and run on.
-
- * main(void) - Entry point.
- *
  * return - return 0 if succesful.
  */
 int main(void)
 {
 	fprintf("Size of a char: %d byte(s)\n", sizeof(char));
 	fprintf("Size of an int: %d byte(s)\n", sizeof(int));
-	fprintf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	fprintf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	fprintf("Size of a long int: %d byte(s)\n", sizeof(long));
+	fprintf("size of a long long int: %d byte(s)\n", sizeof(long long));
 	fprintf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
