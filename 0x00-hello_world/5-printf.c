1f368833107acba1dfe63ef7c076026b8e5cbb2d
alekione <lexa9448@gmail.com>
2022-06-30 21:02:03 +0100

    modified:   5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 /*
  * A program that uses printf function 
  * to display a string on the screen.
+ * main - fuction
+ * return - return 0 if successful.
  */
-#include	<stdio.h>
-
+#include <stdio.h>
 main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
