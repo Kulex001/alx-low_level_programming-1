cc6b3f87ae158af6ea8bb1ba6290208fee44e3a1
alekione <lexa9448@gmail.com>
2022-07-03 14:34:18 +0100

    new file:   102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @void: par indicates null arg
+ *
+ * Return: 0 to indicate success
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 57 && k == 57 && l == 57)
+						break;
+					putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
