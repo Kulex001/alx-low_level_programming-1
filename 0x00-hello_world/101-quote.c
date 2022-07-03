e254dcd33358227cc3efe9ce27862d37c0f1e090
alekione <lexa9448@gmail.com>
2022-07-03 06:49:46 +0100

    new file:   101-quote.c 	modified:   5-printf.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * @void: par indicates null arg
+ *
+ * Return: 1 indicating it's an err
+ */
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fwrite(s, strlen(s), 1, stderr);
+	return (1);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	char s[] = "with proper grammer, but the outcome is a piece of art,";
+
 	printf("%s\n", s);
 
 	return (0);
