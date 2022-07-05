6c2106289546338b97d0ed2645901276577e9bf4
alekione <lexa9448@gmail.com>
2022-07-05 21:27:02 +0100

    new file:   11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,83 @@
+#include "main.h"
+#include <stdlib.h>
+void print_one(int);
+void print_two(int);
+void print_three(int);
+
+/**
+ * print_to_98 - print num upto 98
+ * @n: value given
+ */
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		print_two(n);
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			if (n < 100)
+				print_two(n);
+			else
+				print_three(n);
+		}
+	}
+	else
+	{
+		for (; n <= 98; n++)
+		{
+			int p = abs(n);
+			if (n < 0)
+				_putchar('-');
+			if (p > 9)
+				print_two(p);
+			else
+				print_one(p);
+		}
+	}
+}
+
+/**
+ * print_one - print one char
+ * @n: par passed
+ */
+void print_one(int n)
+{
+	_putchar(n + 48);
+	_putchar(',');
+	_putchar(' ');
+}
+
+/**
+ * print_two - print two char
+ * @n: arg passed
+ */
+void print_two(int n)
+{
+	_putchar((n / 10) + 48);
+	_putchar((n % 10) + 48);
+	if (n == 98)
+		return;
+	_putchar(',');
+	_putchar(' ');
+}
+
+/**
+ * print_three - print 3 char
+ * @n: arg passed
+ */
+void print_three(int n)
+{
+	int p;
+
+	_putchar((n / 100) + 48);
+
+	p = n - ((n / 100) * 100);
+
+	_putchar((p / 10) + 48);
+	_putchar((p % 10) + 48);
+	_putchar(',');
+	_putchar(' ');
+}
