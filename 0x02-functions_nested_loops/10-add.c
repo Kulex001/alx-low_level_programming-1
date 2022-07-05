f3b98ee90f2e05d446c061becc36a6a5dfe79d43
alekione <lexa9448@gmail.com>
2022-07-05 19:43:26 +0100

    new file:   10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two num
+ * @a: int param 1
+ * @b: int param 2
+ *
+ * Return: sum of the two int's
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
