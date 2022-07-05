d79a34aa916833cb479093d3d8d233921a58bd61
alekione <lexa9448@gmail.com>
2022-07-05 19:08:03 +0100

    new file:   6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * abs - compute absolute abs of a num.
+ * @a: num passed
+ *
+ * Return: absolute value
+ */
+int _abs(int a)
+{
+	int b = abs(a);
+	return (b);
+}
