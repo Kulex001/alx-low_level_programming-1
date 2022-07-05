592c0c49786aa0d1b799706c5c48d84739b158e9
alekione <lexa9448@gmail.com>
2022-07-05 22:38:02 +0100

    modified:   8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * jack_bauer - print time 
+ * jack_bauer - print time
  * @void: par inicate null arg
  */
 void jack_bauer(void)
 {
 	int h, i, j, k, l;
 
-	for (h = 0; h <= 23; h++) 
+	for (h = 0; h <= 23; h++)
 	{
 		if (h < 10)
 		{
@@ -20,7 +20,7 @@ void jack_bauer(void)
 			i = h / 10;
 			j = h % 10;
 		}
-		for (k = 0; k <=5; k++)
+		for (k = 0; k <= 5; k++)
 		{
 			for (l = 0; l <= 9; l++)
 			{
