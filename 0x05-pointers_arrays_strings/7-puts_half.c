a77b574c51340ff791c4354415f71a693b2ac045
alekione <lexa9448@gmail.com>
2022-07-11 20:45:13 +0100

    modified:   7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,8 +10,8 @@ void puts_half(char *str)
 	int n, j;
 
 	n = strlen(str);
-	if (n % 2 != 0)
-		n -= 1;
+	if (n % 2 == 1)
+		n = n - 1;
 	for (j = n / 2; j < n; j++)
 		_putchar(str[j]);
 	_putchar('\n');
