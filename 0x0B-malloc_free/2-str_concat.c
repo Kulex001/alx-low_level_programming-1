665ee91015678ded79e97b34621721f3f722fac5
lexa9448 <lexa9448@gmail.com>
2022-07-26 11:17:18 +0100

    modified:   2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * str_concat - concatnate two strings tongether
@@ -9,15 +10,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *str1, *str2;
+	char *str1, *str2, *ptr;
+	int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	i = strlen(s1);
+	j = strlen(s2);
+	ptr = (char *)malloc((i + j) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 	str1 = strdup(s1);
 	str2 = strdup(s2);
 	strcat(str1, str2);
+	ptr = strdup(str1);
 
-	return (str1);
+	return (ptr);
 }
