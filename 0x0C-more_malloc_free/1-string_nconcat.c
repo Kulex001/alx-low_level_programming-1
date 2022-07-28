b6f396383554dc1e26859f5127928be583aee892
lexa9448 <lexa9448@gmail.com>
2022-07-28 20:30:29 +0100

    modified:   1-string_nconcat.c 	new file:   100-realloc.c 	modified:   main.h

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <string.h>
-
+#include <stdlib.h>
 /**
  * string_nconcat - join 2 strings together
  * @s1: string 1
@@ -10,11 +10,12 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str1, *str2;
-	unsigned int len2;
+	char *str, *str1, *str2;
+	unsigned int i, len1, len2;
 
 	str1 = strdup(s1);
 	str2 = strdup(s2);
+	len1 = strlen(str1);
 	len2 = strlen(str2);
 	if (str1 == NULL)
 		str1 = "";
@@ -22,8 +23,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str2 = "";
 	if (n >= len2)
 	{
-		strcat(str1, str2);
-		return (str1);
+		str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+		for (i = 0; i < (len1 + len2); i ++)
+		{
+			if (i < len1)
+				*(str + i) = str1[i];
+			else
+				*(str + i) = str2[i - len1];
+		}
+		*(str + i) = '\0';
+		return (str);
 	}
 	strncat(str1, str2, n);
 	return (str1);
