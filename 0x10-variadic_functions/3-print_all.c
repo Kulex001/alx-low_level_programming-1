3be1ded2419499b51f0bc326d4a2f0eec82e6f9b
lexa9448 <lexa9448@gmail.com>
2022-08-05 14:51:23 +0100

    modified:   2-print_strings.c 	modified:   3-print_all.c 	modified:   variadic_functions.h

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,32 +8,37 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i, j;
 	char *ptr;
 	va_list lst;
 
 	va_start(lst, format);
+	i = 0, j = 0;
 	while (format && format[i])
 	{
-		if (i && (format[i] == 'c' || format[i] == 'i' ||
+		if (i && j == 1 && (format[i] == 'c' || format[i] == 'i' ||
 					format[i] == 'f' || format[i] == 's'))
 			printf(", ");
 		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(lst, int));
+				j = 1;
 				break;
 			case 'i':
 				printf("%d", va_arg(lst, int));
+				j = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(lst, double));
+				j = 1;
 				break;
 			case 's':
 				ptr = va_arg(lst, char *);
 				if (ptr == NULL)
-					ptr = "nil";
+					ptr = "(nil)";
 				printf("%s", ptr);
+				j = 1;
 				break;
 		}
 		i++;
